07bff9f8e60eb77a752b0948499293af2f43d6b5
Brian Wang <brianwang7102@gmail.com>
2025-09-10 21:04:58 -0400

    add circle element to separate class

--- src/components/bg1.ts
+++ src/components/bg1.ts
@@ -1,6 +1,7 @@
 import { LitElement, html, css, type PropertyDeclarations, type CSSResultGroup, type PropertyValues } from "lit";
 import "./circle.js"
 import { randomRange } from "../utilities/random.js";
+import { Circle, type circleConfig } from "./circle.js";
 
 type circleData = { 
     element: SVGCircleElement, 
@@ -14,7 +15,7 @@ type circleData = {
 
 export class BG1 extends LitElement {
     svgContent: HTMLElement | null = null;
-    circles: circleData[] = [];
+    circles: Circle[] = [];
     intervalID: number = 0;
     numCircles: number = 10;
     validColors: string[] = [];
@@ -36,13 +37,12 @@ export class BG1 extends LitElement {
             z-index: 2;
         }
 
-        .svg {
+        .background {
+            display: flex;
+            width: 100%;
+            height: 100%
             position: absolute;
             z-index: 1;
-        }
-
-        .circle {
-            backdrop-filter: blur(16px);
         }
     `;
 
@@ -55,106 +55,104 @@ export class BG1 extends LitElement {
         super();
         this.className = "";
         this.numCircles = parseInt(this.getAttribute("instances") ?? "10");
-        this.validColors = this.getAttributeValue("palette", "")?.split(" ").filter((candidate) => {
+        this.validColors = this.tryGetAttribute("palette", "")?.split(" ").filter((candidate) => {
             return candidate.match(/^#?([0-9a-fA-F]{6}|[0-9a-fA-F]{3})$/);
-        })
-
-        console.log(this.validColors);
+        });
     }
 
     buildCSS() {
         return
     }
 
-    getAttributeValue(name: string, fallback: string) {
+    tryGetAttribute(name: string, fallback: string) {
         return this.getAttribute(name) ?? fallback
     }
 
-    protected render(): unknown {
-        return html`
-            <div class="body ${this.className}" id="body">
-                <div class="content">
-                    <slot></slot>
-                </div>
-                <svg class="svg" id="svg-content" width="100%" height="100%" xmls="http://www.w3.org/2000/svg" style="background-color: ${this.getAttributeValue('background', '#292929')}"/></svg>
-            </div>`;
-    }
-
+    
     protected firstUpdated(_changedProperties: PropertyValues): void {
-        this.svgContent = this.shadowRoot!.getElementById("svg-content")!;
+        this.svgContent = this.shadowRoot!.getElementById("svg-content");
+
         for (let i = 0; i < this.numCircles; i++) {
             const circle = this.spawnCircle();
-            this.svgContent.appendChild(circle.element);
+            this.svgContent!.appendChild(circle.element);
             this.circles.push(circle);
         }
-
+        
         this.intervalID = setInterval(() => {
-            const bounds = this.svgContent!.getBoundingClientRect();
-            this.circles = this.circles.filter((circle) => {
-                this.updateCircle(circle);
+            if (!this.svgContent) return;
 
+            const bounds = this.svgContent.getBoundingClientRect();
+            this.circles = this.circles.filter((circle) => {
+                circle.step(40.667);
+                
                 if (circle.posX + circle.radius < 0 ||
                     circle.posX - circle.radius > bounds.width ||
                     circle.posY + circle.radius < 0 ||
                     circle.posY - circle.radius > bounds.height
                 ) {
-                    this.svgContent?.removeChild(circle.element);
+                    this.svgContent!.removeChild(circle.element);
                     return false;
                 }
-
+                
                 return true;
             });
-
+            
             while( this.circles.length < this.numCircles) {
                 const circle = this.spawnCircle();
-                this.svgContent?.appendChild(circle.element);
+                this.svgContent.appendChild(circle.element);
                 this.circles.push(circle);
             }
         }, 40.667);
     }
-
+    
     disconnectedCallback(): void {
         super.disconnectedCallback();
-
+        
         clearInterval(this.intervalID);
     }   
-
+    
     updateCircle(data: circleData) {
-        data.posX += data.delX / 60;
-        data.posY += data.delY / 60;
         
-        if (data.opacity != 50) {
-            data.opacity += 0.5;
-            data.element.setAttribute("opacity", `${data.opacity}%`);
-        }
-
-        data.element.setAttribute("cx", data.posX.toString());
-        data.element.setAttribute("cy", data.posY.toString());
     }
-
+    
     spawnCircle() {
-        const rect = this.svgContent!.getBoundingClientRect();
+        const rect = this.getBoundingClientRect();
         
-        const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        const circleData: circleData = {
-            element: circle,
-            posX: randomRange(0, rect.width),
-            posY: randomRange(0, rect.height),
-            delX: (Math.random() - 0.5) * 20,
-            delY: (Math.random() - 0.5) * 20,
-            radius: randomRange(20, 80),
-            opacity: 0
+        const config: circleConfig = {
+            posX: {
+                min: 0,
+                max: rect.width
+            },
+            posY: {
+                min: 0,
+                max: rect.height
+            },
+            vel: {
+                min: 10,
+                max: 30
+            },
+            radius: {
+                min: 10,
+                max: 30
+            },
+            color: this.validColors[Math.floor(Math.random() * this.validColors.length)] ?? "#292929"
         }
 
-        circle.setAttribute("fill", "#292929");
-        circle.setAttribute("opacity", `${circleData.opacity}%`);
-        circle.setAttribute("cx", circleData.posX.toString());
-        circle.setAttribute("cy", circleData.posY.toString());
-        circle.setAttribute("r" , circleData.radius.toString());
-        circle.setAttribute("fill", this.validColors[Math.floor(Math.random() * this.validColors.length)] ?? "#292929");
-        circle.setAttribute("class", "circle")
-
-        return circleData;
+        return new Circle(config);
+    }
+    
+    protected render(): unknown {
+        return html`
+            <div class="body ${this.className}" id="body" style="background-color: ${this.tryGetAttribute("background", "#292929")}">
+                <div class="content">
+                    <slot></slot>
+                </div>
+                <svg class="svg" id="svg-content" width="100%" height="100%" xmls="http://www.w3.org/2000/svg" style="background-color: ${this.tryGetAttribute('background', '#292929')}; "/>
+                    <filter>
+                        
+                    </filter>
+                </svg>
+            </div>`;
     }
 }
 customElements.define("bg-1", BG1);
--- src/components/circle.ts
+++ src/components/circle.ts
@@ -1,30 +1,71 @@
-import { LitElement, html, type PropertyValues } from "lit";
+import { LitElement, html, type PropertyValues, svg } from "lit";
+import { randomRange } from "../utilities/random.js";
 
-export class Circle extends LitElement {
-    element: HTMLElement | null = null;
-
-    static get observedAttributes() {
-        return ["radius", "color"]
+export type circleConfig = {
+    posX: {
+        min: number,
+        max: number
+    },
+    posY: {
+        min: number,
+        max: number,
     }
-
-    constructor() {
-        super();
-        this.render();
-    }
-
-    render() {
-        return html`
-            <circle id="child" fill="black"/>
-        `
-    }
-
-    protected firstUpdated(_changedProperties: PropertyValues): void {
-        this.element = this.shadowRoot?.getElementById("child")!;
-        this.element.setAttribute("cx", "240");
-        this.element.setAttribute("cy", "240");
-        this.element.setAttribute("r" , "25");
-    }
+    vel: {
+        min: number,
+        max: number
+    },
+    radius: {
+        min: number,
+        max: number
+    },
+    color: string
 }
 
-customElements.define("bg1-circle", Circle)
+export class Circle {
+    element: SVGCircleElement
+    radius: number = 10;
+    posX: number = 0;
+    posY: number = 0;
+    velX: number = 0;
+    velY: number = 0;
+    opacity: number = 0;
+    color: string = "#292929";
 
+    constructor(config: circleConfig) {
+        this.posX = randomRange(config.posX.min, config.posX.max);
+        this.posY = randomRange(config.posY.min, config.posY.max);
+
+        const theta = randomRange(0, 2 * 3.14159);
+        const vel = randomRange(config.vel.min, config.vel.max);
+        this.velX = Math.cos(theta) * vel;
+        this.velY = Math.sin(theta) * vel;
+
+        this.color = config.color
+
+
+        const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+
+        circle.setAttribute("fill", "#292929");
+        circle.setAttribute("opacity", `${this.opacity}%`);
+        circle.setAttribute("cx", this.posX.toString());
+        circle.setAttribute("cy", this.posY.toString());
+        circle.setAttribute("r" , randomRange(config.radius.min, config.radius.max).toString());
+        circle.setAttribute("fill", this.color);
+        circle.setAttribute("class", "circle")
+
+        this.element = circle;
+    }
+
+    step(dt: number) {
+        this.posX += this.velX / 60;
+        this.posY += this.velY / 60;
+        
+        if (this.opacity != 50) {
+            this.opacity += 0.5;
+            this.element.setAttribute("opacity", `${this.opacity}%`);
+        }
+        
+        this.element.setAttribute("cx", this.posX.toString());
+        this.element.setAttribute("cy", this.posY.toString());
+    }
+}
